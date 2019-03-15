5602c7c268d9e39b335a4e8e657abfa0beb898ab
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-03-15 10:16:09 +0700

    Finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,12 +31,12 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
-        axios.get('https://react-burger-app-7cf4c.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data});
-            }).catch(error => {
-            this.setState({error: true});
-        });
+        // axios.get('https://react-burger-app-7cf4c.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ingredients: response.data});
+        //     }).catch(error => {
+        //     this.setState({error: true});
+        // });
     }
 
     updatePurchaseState(ingredients) {
