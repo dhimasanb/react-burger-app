4c121788fe83f4b8af4f2476aeaa53a5aac9820d
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-03-15 08:18:23 +0700

    Handling user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -98,6 +98,18 @@ class ContactData extends Component {
             });
     };
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormELement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormELement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormELement;
+        this.setState({orderForm: updatedOrderForm});
+    };
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -113,6 +125,7 @@ class ContactData extends Component {
                     elementType={formElement.config.elementType}
                     elementConfig={formElement.config.elementConfig}
                     value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)}
                 />
             ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
